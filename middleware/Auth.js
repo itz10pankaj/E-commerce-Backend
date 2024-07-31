2d7cc2b378ab8649c3cef4687d788ca63e954c47
Pankaj <pgarg9355@gmail.com>
2024-08-01 02:13:43 +0530

    Ensure middleware/Auth.js file is added

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,27 +1,28 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncError = require("./catchAsyncError");
-const JWT=require("jsonwebtoken")
-const User=require("../models/userModel");
+const JWT = require("jsonwebtoken")
+const User = require("../models/userModel");
+
 const isAuthenticatedError = catchAsyncError(async (req, res, next) => {
-    const {token} = req.cookies;
+    const { token } = req.cookies;
     // console.log(token);
 
-    if(!token){
-        return next(new ErrorHandler("Please login to access this resourse",401))
+    if (!token) {
+        return next(new ErrorHandler("Please login to access this resourse", 401))
     }
-    const decodeData=JWT.verify(token,process.env.JWT_SECRET);
+    const decodeData = JWT.verify(token, process.env.JWT_SECRET);
 
-    req.user=await User.findById(decodeData.id)
+    req.user = await User.findById(decodeData.id)
     next();
 });
 
-const authorizeRole=(...roles)=>{
-    return (req,res,next)=>{
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorHandler(`ROLE:${req.user.role} is not allowed to acces data`,403))
+const authorizeRole = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`ROLE:${req.user.role} is not allowed to acces data`, 403))
         }
         next();
     }
 }
 
-module.exports = {isAuthenticatedError,authorizeRole};
+module.exports = { isAuthenticatedError, authorizeRole };
